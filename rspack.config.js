4f1af7a76311107a7555b4abad2532bcf9438db1
Marcus Vinícius <marcusvinici25@gmail.com>
2025-01-09 11:51:24 -0300

    rm remote

--- rspack.config.js
+++ rspack.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
     compress: true,
   },
   output: {
-    path: path.resolve(__dirname, './remote/dist/'),
+    path: path.resolve(__dirname, './dist/'),
     uniqueName: 'remote',
     publicPath: `${process.env.PUBLIC_PATH_URL}`,
     clean: true,
